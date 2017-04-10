9e2ffa78660ec144601da0081dd66c8ff526b6c3
Julio Lima <jclima27@yahoo.com.br>
2017-04-10 19:03:40 -0300

    Adionando EditoraBean, EditoraDAO e Model Editora

---
+++ Projeto-EcommerceJSF/src/br/com/ecommerce/modelo/Editora.java
@@ -0,0 +1,50 @@
+package br.com.ecommerce.modelo;
+
+public class Editora {
+	int idEditora;
+	String nome;
+	String cnpj;
+	String endereco;
+	String telefone;
+	
+	public int getIdEditora() {
+		return idEditora;
+	}
+
+	public void setIdEditora(int idEditora) {
+		this.idEditora = idEditora;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+}	
